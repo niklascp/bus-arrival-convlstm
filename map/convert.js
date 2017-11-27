53af955e2fcdf6edb94e8de2714a978fabf6c6f0
niklascp <niklascp@gmail.com>
2017-11-27 10:33:18 +0100

    Fix path to map.html.

--- map/convert.js
+++ map/convert.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setViewport({width: 800, height: 1000});
-  await page.goto('file://C:/Users/ncp/Jupyter/bus-arrival-convlstm/map/map.html');
+  await page.goto('file://./map.html');
   await page.screenshot({path: 'map.png'});
 
   await browser.close();
